bedf1accffc7dbae8fe9b1c45050d2e09b61f4b9
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-03-10 10:29:57 +0000

    Add files via upload

---
+++ build/precache-manifest.03610972ebaa45ffd92aa1ee676905c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a4219ab51949ab9ae059",
+    "url": "/static/css/main.8b73820b.chunk.css"
+  },
+  {
+    "revision": "a4219ab51949ab9ae059",
+    "url": "/static/js/main.a4219ab5.chunk.js"
+  },
+  {
+    "revision": "98fae294dabfb7543782",
+    "url": "/static/js/1.98fae294.chunk.js"
+  },
+  {
+    "revision": "80222a1bb256302cf2c5",
+    "url": "/static/js/2.80222a1b.chunk.js"
+  },
+  {
+    "revision": "b600c0325db6bc8bf28f",
+    "url": "/static/js/runtime~main.b600c032.js"
+  },
+  {
+    "revision": "1d0b01927e5ec02c3292b831253e6a2a",
+    "url": "/index.html"
+  }
+];
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Zahra Olanrewaju</h2>
+        <h4>HomeWork</h4>
+      </header>
+    </div>
+  );
+}
+
+export default App;
