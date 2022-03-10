cc0ab6aca6905ec55bee0649ec76f3cb3f005734
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-03-10 13:40:44 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ function App() {
       <header className="App-header">
         <h2>Zahra Olanrewaju</h2>
         <h4>HomeWork</h4>
+        <small>
+          <a href="https://github.com/zahrabola/my-bola-app" target="_blank">
+            Open-source code
+          </a>
+          <p>By Zahra Olanrewaju</p>
+        </small>
       </header>
     </div>
   );
